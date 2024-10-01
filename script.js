2a34f2e056afbe1edb447008b27faee46b031f9b
Asapro2024 <computerayo123@gmail.com>
2024-10-01 08:41:57 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("red-btn").addEventListener("click", function() {
+    document.getElementById("rect1").style.backgroundColor = "red";
+  });
+  
+  document.getElementById("blue-btn").addEventListener("click", function() {
+    document.getElementById("rect2").style.backgroundColor = "rgb(12, 4, 124)";
+  });
+  
+  
